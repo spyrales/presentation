6c1919abd74c7fceecb1b6ed0529b6e17e9d0a66
Romain Lesur <romain.lesur@gmail.com>
2020-04-09 23:06:37 +0200

    fix build pdf script

--- _build_pdf.R
+++ _build_pdf.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
 pages <- list.files(path = "_site", pattern = ".html$", full.names = TRUE)
-pages <- grep("index.html$", f, invert = TRUE, value = TRUE)
+pages <- grep("index.html$", pages, invert = TRUE, value = TRUE)
 for (page in pages) {
   pagedown::chrome_print(page)
 }
